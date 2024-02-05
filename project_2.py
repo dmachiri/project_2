ae24dab718bda4f707c1819b280e811e78bb2a06
dmachiri <139544736+dmachiri@users.noreply.github.com>
2024-02-05 11:50:51 +0300

    Update project_2.py

--- project_2.py
+++ project_2.py
@@ -1,13 +1,14 @@
 import streamlit 
 import pandas 
- 
+import matplotlib.pyplot as plt
+
 streamlit.write("""
 # My first app
 Density of states*
 """)
 
 streamlit.image("dos", caption="Density of states for FCC Aluminium metal", output_format="auto" )
-
+fig = plt.figure() 
 
 df = pandas.read_csv("al.csv")
 streamlit.line_chart(df)
