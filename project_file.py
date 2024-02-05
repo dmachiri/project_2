69dec81f4072f91c3130d1e002d165adb90c36f0
dmachiri <139544736+dmachiri@users.noreply.github.com>
2024-02-05 12:46:22 +0300

    Update project_file.py

--- project_file.py
+++ project_file.py
@@ -2,18 +2,9 @@
 
 import streamlit as st
 
-#from PIL 
-import Image
-
-
-
-#opening the image
+from PIL import Image
 
 image = Image.open('MAPbBr_3.eps')
-
-
-
-#displaying the image on streamlit app
 
 st.image(image, caption='Band Structure and Density of states for Methyl ammonia lead bromide perovskite')
 
